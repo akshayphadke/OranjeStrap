d3e1e1f81038590253af24e79bd4c44fca9328ed
akshay.phadke <akshay.phadke@gmail.com>
2013-10-20 14:15:36 -0700

    changes to 404 page…changing colors of tags, date

--- content.php
+++ content.php
@@ -9,9 +9,9 @@
 		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 
 		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
+		<span class="entry-meta">
 			<?php OranjeStrap_posted_on(); ?>
-		</div><!-- .entry-meta -->
+		</span><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
@@ -38,25 +38,21 @@
 				$categories_list = get_the_category_list( __( ', ', 'OranjeStrap' ) );
 				if ( $categories_list && OranjeStrap_categorized_blog() ) :
 			?>
-			<span class="cat-links">
-				<?php printf( __( 'Posted in %1$s', 'OranjeStrap' ), $categories_list ); ?>
-			</span>
+			<!-- <div class="cat-links">
+				<?php printf( __( 'Category: %1$s', 'OranjeStrap' ), $categories_list ); ?>
+			</div> -->
 			<?php endif; // End if categories ?>
-
+			
 			<?php
 				/* translators: used between list items, there is a space after the comma */
-				$tags_list = get_the_tag_list( '', __( ', ', 'OranjeStrap' ) );
+				$tags_list = get_the_tag_list( '', __( '  | ', 'OranjeStrap' ) );
 				if ( $tags_list ) :
 			?>
 			<span class="tags-links">
-				<?php printf( __( 'Tagged %1$s', 'OranjeStrap' ), $tags_list ); ?>
+				<?php printf( __( 'Tags: %1$s', 'OranjeStrap' ), $tags_list ); ?>
 			</span>
 			<?php endif; // End if $tags_list ?>
 		<?php endif; // End if 'post' == get_post_type() ?>
-
-		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'OranjeStrap' ), __( '1 Comment', 'OranjeStrap' ), __( '% Comments', 'OranjeStrap' ) ); ?></span>
-		<?php endif; ?>
 
 		<?php edit_post_link( __( 'Edit', 'OranjeStrap' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-meta -->
--- inc/template-tags.php
+++ inc/template-tags.php
@@ -182,7 +182,7 @@ function OranjeStrap_posted_on() {
 		esc_html( get_the_modified_date() )
 	);
 
-	printf( __( '<span class="posted-on">Posted on %1$s</span><span class="byline"> by %2$s</span>', 'OranjeStrap' ),
+	printf( __( '<span class="posted-on">%1$s</span><span class="byline"> by %2$s</span>', 'OranjeStrap' ),
 		sprintf( '<a href="%1$s" rel="bookmark">%2$s</a>',
 			esc_url( get_permalink() ),
 			$time_string
