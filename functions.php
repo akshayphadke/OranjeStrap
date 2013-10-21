0eb1b4df4d02c34df8ea88a938f522cce6a968eb
akshay.phadke <akshay.phadke@gmail.com>
2013-10-20 19:11:44 -0700

    added function to get current page url, removed ubnuntu font

--- functions.php
+++ functions.php
@@ -99,6 +99,22 @@ function OranjeStrap_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'OranjeStrap_scripts' );
 
+function curPageURL() {
+	$pageURL = 'http';
+	if ($_SERVER["HTTPS"] == "on") {
+		$pageURL .= "s";
+	}
+	
+	$pageURL .= "://";
+	if ($_SERVER["SERVER_PORT"] != "80") {
+		$pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+ 	} else {
+ 		$pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+ 	}
+	
+	return $pageURL;
+}
+
 /**
  * Implement the Custom Header feature.
  */
