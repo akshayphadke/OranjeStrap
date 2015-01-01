92214086a82ee21b64336726a18c3db54747b5f4
akshay.phadke <akshay.phadke@gmail.com>
2014-12-31 16:43:13 -0800

    trial?
    trial?

---
+++ page-landing.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Template Name: My Custom Page
+*/
+/**
+ * @package OranjeStrap
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			Here is the custom page.
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
